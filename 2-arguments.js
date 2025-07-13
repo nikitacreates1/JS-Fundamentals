e6f71fba47ec1d5e2142d323b2ba08378c1e7654
Nikita de Huis <Kiastewart001@gmail.com>
2025-07-13 16:58:05 +0200

    Add argument count script

---
+++ 2-arguments.js
@@ -0,0 +1,9 @@
+const args = process.argv.slice(2); // get arguments passed after node script.js
+
+if (args.length === 0) {
+  console.log("No argument");
+} else if (args.length === 1) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
+}
